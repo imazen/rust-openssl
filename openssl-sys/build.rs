6d4497935b3205310186b67c4359e7b68e1a910b
Nathanael Jones <nathanael.jones@gmail.com>
2016-09-03 14:41:24 -0600

    Don't add the entire system lib folder to the link path

--- openssl-sys/build.rs
+++ openssl-sys/build.rs
@@ -14,8 +14,33 @@ fn main() {
     if lib_dir.is_none() && include_dir.is_none() {
         // rustc doesn't seem to work with pkg-config's output in mingw64
         if !target.contains("windows") {
-            if let Ok(info) = pkg_config::find_library("openssl") {
-                // avoid empty include paths as they are not supported by GCC
+
+            let mut conf = ::pkg_config::Config::new();
+            // Disable writing to STDOUT from pkg_config directly; we need to 
+            // take a less clobbering approach
+            conf.cargo_metadata(false);
+            //Probe for openssl
+            if let Ok(info) = conf.probe("openssl") {
+                //We don't need to consider frameworks -F
+
+                let ref pkg_libs = info.libs;
+                let ref pkg_link_paths = info.link_paths;
+                // handle (-l)
+                for libname in pkg_libs {
+                    // We asked pkg-config to include system lib folders, 
+                    // So it's up to use to prevent clobbering
+                    // rust link ordering is non-deterministic
+                    for path in pkg_link_paths{
+                        println!("cargo:rustc-link-search=native={}/lib{}*", path.to_str().unwrap(), libname);
+                    }
+
+                    //We aren't supporting static linking here
+                    //but assuming this is OK
+                    println!("cargo:rustc-link-lib={}", libname);
+
+                }                   
+
+                // handle (-I), avoid empty include paths as they are not supported by GCC
                 if info.include_paths.len() > 0 {
                     let paths = env::join_paths(info.include_paths).unwrap();
                     println!("cargo:include={}", paths.to_str().unwrap());
